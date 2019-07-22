ef48ac79d22996a96924a25546e1d22053e6dd56
Essam Salah <essam@hashfood.com>
2019-07-22 14:48:43 +0200

    use env vars

--- server/config.php
+++ server/config.php
@@ -2,28 +2,28 @@
 
 /**
  * Class for storing global variables.
- *
+ * Essam: Modifed to use Environment Variables
  * @author	Humphrey Shotton
- * @version	1.1 (2014-03-21)
+ * @version	1.2 (2019-07-22)
  */
 
 class Conf{
 	// MySQL Database Settings.
-	const DB_HOST = "127.0.0.1";
-	const DB_USER = "root";
-	const DB_PASS = "";
-	const DB_DATABASE = "raspi";
+	const DB_HOST = $_ENV["DB_HOST"];
+	const DB_USER = $_ENV["DB_USER"];
+	const DB_PASS = $_ENV["DB_PASS"];
+	const DB_DATABASE = $_ENV["DB_DATABASE"];
 	
 	// Base URL on the server.
-	const URL_BASE = "/pipark/server/";
+	const URL_BASE = $_ENV["URL_BASE"];
 	
 	// The password for the pi's to submit data.
-	const PI_PASSWORD = 'exeterPiPark';
+	const PI_PASSWORD = $_ENV["PI_PASSWORD"];
 	
 	// Title of the website.
-	const TITLE = 'PiPark';
+	const TITLE = $_ENV["TITLE"];
 	
 	// Google maps API key
-	const MAPS_API_KEY = 'AIzaSyCxTGtd15r1PXxGyPSA17YjoPcN73ENmPc';
+	const MAPS_API_KEY = $_ENV["MAPS_API_KEY"];
 }
 ?>
